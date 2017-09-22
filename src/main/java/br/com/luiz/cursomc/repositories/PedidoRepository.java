611e2a7972b2d014a5dbf8c34f0c849dc0b33a18
LuizIFTM <luizx2012x@gmail.com>
2017-09-21 21:18:23 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/br/com/luiz/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package br.com.luiz.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.luiz.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
