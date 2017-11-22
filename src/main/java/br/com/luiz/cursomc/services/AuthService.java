06d1fac7d455eedb8e6409bb249ee75c8cc629c3
LuizIFTM <luizx2012x@gmail.com>
2017-11-22 10:08:59 -0200

    Esqueci a senha

---
+++ src/main/java/br/com/luiz/cursomc/services/AuthService.java
@@ -0,0 +1,63 @@
+package br.com.luiz.cursomc.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.luiz.cursomc.domain.Cliente;
+import br.com.luiz.cursomc.repositories.ClienteRepository;
+import br.com.luiz.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository; 
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
+	private Random rand = new Random();
+	
+	@Autowired
+	private EmailService emailService;
+	
+	public void sendNewPassword(String email) {
+		
+		Cliente cliente = clienteRepository.findByEmail(email);
+		
+		if(cliente == null) {
+			throw new ObjectNotFoundException("EMAIL NAO ENCONTRADO");
+		}
+		
+		String newPass = newPassword();
+		cliente.setSenha(pe.encode(newPass));
+		
+		clienteRepository.save(cliente);
+		emailService.sendNewPasswordEmail(cliente, newPass);
+	}
+
+	private String newPassword() {
+		char[] vet = new char[10];
+		for(int i = 0; i < 10; i++) {
+			vet[i] = randomChar();
+		}
+		return new String(vet);
+	}
+
+	private char randomChar() {
+		int opt = rand.nextInt(3);
+		if (opt == 0) {
+			return (char) (rand.nextInt(10) + 48);
+		}
+		else if (opt == 1) {
+			return (char) (rand.nextInt(26) + 65);
+		}
+		else {
+			return (char) (rand.nextInt(26) + 97);
+		}
+	}
+
+}
--- src/main/java/br/com/luiz/cursomc/services/EmailService.java
+++ src/main/java/br/com/luiz/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package br.com.luiz.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.luiz.cursomc.domain.Cliente;
 import br.com.luiz.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
